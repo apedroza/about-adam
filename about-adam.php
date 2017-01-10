300f3b84acf6dd6b7cbabf4208f9df25b00cca49
Jennifer Minnich <jminniecc@gmail.com>
2017-01-10 09:16:22 -0700

    commit new "about Adam"

--- about-adam.php
+++ about-adam.php
@@ -5,7 +5,11 @@
 	</head>
 	<body>
 		<img src="img/sandia_peak_abqnight.jpg">
-		<p>There have been a lot of experiences and some people that have influenced me through-out my life. I am
+		<p>Hey, how's it going my name is Adam Pedroza and this is a pic I took of Tokyo, Japan when I was traveling for my post-grad thesis (in modern Japanese architecture).
+		I have been certified as a "Genius" since I was three years old. Life as a genius has it's ups and downs, but generally it's pretty cool. My day to day consists of being a total badass and accomplishing all my hopes and dreams.
+		</p>
+
+			<!––There have been a lot of experiences and some people that have influenced me through-out my life. I am
 			currently a full-time student at Central New Mexico Community College in Albuquerque, NM, which is where I grew up.
 			This is my first year continuing my education since high school, mostly because I was no satisfied with my routine and
 			employment situation, so I thought this would help me get out of that mindset and get a fresh perspective. As the third
@@ -13,6 +17,7 @@
 			to meet through-out my life. I have always been a curious and inquisative person, that is OK with doing things out of
 			the "norm". This mindset has allowed me to experience life in different ways but has also been a distraction in
 			accomplishing my long-term goals. With that, I plan on taking that same mindset but with a more responsible approach
-			and accomplish my "Hopes and Dreams"<</p>
+			and accomplish my "Hopes and Dreams"-->
+
 	</body>
 </html>
