1e4c55956329389fd89d3e5cefed1dcbb89cc4ac
Adam Pedroza <apedroza6@cnm.edu>
2017-01-10 08:51:58 -0700

    pushing to github

---
+++ about-adam.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>This Is My First Site Bout ME</title>
+	</head>
+	<body>
+		<img src="img/sandia_peak_abqnight.jpg">
+		<p>There have been a lot of experiences and some people that have influenced me through-out my life. I am
+			currently a full-time student at Central New Mexico Community College in Albuquerque, NM, which is where I grew up.
+			This is my first year continuing my education since high school, mostly because I was no satisfied with my routine and
+			employment situation, so I thought this would help me get out of that mindset and get a fresh perspective. As the third
+			of four siblings, my older brother has been a positive influence, along with a few other mentors I have been fortunate
+			to meet through-out my life. I have always been a curious and inquisative person, that is OK with doing things out of
+			the "norm". This mindset has allowed me to experience life in different ways but has also been a distraction in
+			accomplishing my long-term goals. With that, I plan on taking that same mindset but with a more responsible approach
+			and accomplish my "Hopes and Dreams"<</p>
+	</body>
+</html>
